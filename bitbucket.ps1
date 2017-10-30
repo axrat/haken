3d290e88313d64bda4bd9c219e27fdc3b698551c
onoie <onoie3@gmail.com>
2017-10-30 13:57:59 +0900

    fast commit

--- bitbucket.ps1
+++ bitbucket.ps1
@@ -1,7 +1,8 @@
-Write-Host "HelloWorld"
+Write-Host $pwd
 $url = $env:BITBUCKET_URL
 $cred = $env:BITBUCKET_USER + ":" + $env:BITBUCKET_PASS
 $enc_cred = [convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($cred))
 $webclient = new-object System.Net.WebClient
 $webclient.Headers.Add("Authorization","Basic $enc_cred")
 $webclient.DownloadFile($url, "master.zip")
+"complete"
