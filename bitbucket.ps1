4e6dc164cd2d70b9cdcbb71ffa692b54db214220
onoie <onoie3@gmail.com>
2017-10-30 17:26:28 +0900

    Update bitbucket.ps1

--- bitbucket.ps1
+++ bitbucket.ps1
@@ -1,8 +1,47 @@
 Write-Host "Start"
+
+
+#"https://bitbucket.org/user/repo/get/master.zip"
 $url = $env:BITBUCKET_URL
+#"User:Pass"
 $cred = $env:BITBUCKET_USER + ":" + $env:BITBUCKET_PASS
 $enc_cred = [convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($cred))
 $webclient = new-object System.Net.WebClient
 $webclient.Headers.Add("Authorization","Basic $enc_cred")
 $webclient.DownloadFile($url, (join-Path $pwd "master.zip"))
+
+function mkdirp($path){
+  If((test-path $path)){
+    Remove-Item -path $path -recurse -force  
+  }
+  New-Item $path -type directory -Force 
+}
+
+
+mkdirp (Join-Path $pwd "unzip")
+mkdirp (Join-Path $pwd "source")
+
+$download = (Join-Path $pwd "master.zip")
+$dest = (Join-Path $pwd "unzip")
+$out = (Join-Path $pwd "source")
+function Expand-ZIPFile($file, $destination){
+  $shell = new-object -com shell.application
+  $zip= $shell.NameSpace($file)
+  foreach($item in $zip.items()){
+    $shell.Namespace($destination).copyhere($item)
+  }
+}
+Expand-ZipFile $download -destination $dest
+$content = Get-ChildItem -Path $dest -Recurse | ?{ $_.PSIsContainer }
+foreach ($line in $content){
+    if ( [string]::IsNullOrEmpty($line) ) {
+        "NULL"
+    } else {
+       Write-Host "HIT"
+       break
+    }
+}
+Expand-ZIPFile (Join-Path $dest $line) -destination $out
+
+
 Write-Host "Complete"
